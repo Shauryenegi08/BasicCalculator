9e62c3e4e3e61c4408b1016d756e10980f2d8883
Shauryenegi08 <negishaurye@gmail.com>
2025-07-17 19:21:37 +0530

    correction in the code

--- SourceCode/script.js
+++ SourceCode/script.js
@@ -1,25 +1,25 @@
 let currentExpression = '';
 
 function appendNumber(number) {
-  currentExpression += number;
-  document.getElementById('result').value = currentExpression;
+    currentExpression += number;
+    document.getElementById('result').value = currentExpression;
 }
 
 function appendOperator(operator) {
-  currentExpression += ` ${operator} `;
-  document.getElementById('result').value = currentExpression;
+    currentExpression += ` ${operator} `;
+    document.getElementById('result').value = currentExpression;
 }
 
 function calculateResult() {
-  try {
-    currentExpression = eval(currentExpression.replace('+', '/').replace('x', '*'));
-    document.getElementById('result').value = currentExpression;
-  } catch {
-    document.getElementById('result').value = 'Error';
-  }
+    try {
+        currentExpression = eval(currentExpression.replace('÷', '/').replace('×', '*'));
+        document.getElementById('result').value = currentExpression;
+    } catch {
+        document.getElementById('result').value = 'Error';
+    }
 }
 
 function clearResult() {
-  currentExpression = '';
-  document.getElementById('result').value = '';
+    currentExpression = '';
+    document.getElementById('result').value = '';
 }
